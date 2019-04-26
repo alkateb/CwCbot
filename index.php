0e77468d2f41c01da0e17aa862162f8a44ebc94f
alkateb <36197396+alkateb@users.noreply.github.com>
2019-04-26 11:44:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
-  
+  /*
     $text = $json->result->parameters->text;
 	
 	  $ch = curl_init(); 
@@ -16,6 +16,25 @@ $result = curl_exec($ch);
 $vars = json_decode($result, true);
  
             $speech = $result;
+  */
+  $service = $json->result->parameters->services_entities;
+    $city = $json->result->parameters->cities;
+    $help = $json->result->parameters->HelpEntities;
+ 
+ 
+    $ch = curl_init(); 
+//curl_setopt($ch, CURLOPT_URL, 'http://88.247.29.227/test.php?link=https://admin-turkey.servicesadvisor.org/en/api/v1.0/service_location?fields=serviceName,endDate%26filter[region]=464%26filter[servicesProvided]='.$_GET['link']); 
+curl_setopt($ch, CURLOPT_URL, 'http://help.unhcr.org/turkey/wp-json/wp/v2/pages/'.$help); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+ 
+$vars = json_decode($result, true);
+$vars1 = json_decode($result);
+$varsDate = json_decode($result, true);
+ 
+//echo $vars1->content->rendered;
+$eResult= $vars1->content->rendered;
+  
   
   
     $response = new \stdClass();
