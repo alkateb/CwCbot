9d147297f4e4197d267cf8988fac3e71d9ba5110
alkateb <36197396+alkateb@users.noreply.github.com>
2019-04-26 11:57:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,27 +9,15 @@ if($method == 'POST'){
   
     $text = $json->result->parameters->text;
 	 $help = $json->result->parameters->HelpEntities;
-/*	
-	$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, 'http://88.247.29.227/test.php?link=https://admin-turkey.servicesadvisor.org/en/api/v1.0/service_location?fields=serviceName%26filter[id]=319'); 
+	  $ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, 'http://help.unhcr.org/turkey/wp-json/wp/v2/pages/'.$help); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 $vars = json_decode($result, true);
  
-            $speech = $help.' this is help entity';
-  */
-  $ch = curl_init(); 
-  curl_setopt($ch, CURLOPT_URL, 'http://help.unhcr.org/turkey/wp-json/wp/v2/pages/'.$help); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($ch);
- 
-$vars = json_decode($result, true);
-$vars1 = json_decode($result);
-$varsDate = json_decode($result, true);
- 
-//echo $vars1->content->rendered;
-$eResult= $vars1->content->rendered;
-$speech=$eResult;
+            $speech = $result;
+  
+
   
     $response = new \stdClass();
     $response->speech = $speech;
