cf03faa67578faa9d5d335381d110c8c8206e9e8
alkateb <36197396+alkateb@users.noreply.github.com>
2019-04-26 12:01:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if($method == 'POST'){
     $text = $json->result->parameters->text;
 	 $help = $json->result->parameters->HelpEntities;
 	  $ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, 'http://help.unhcr.org/turkey/wp-json/wp/v2/pages/'.$help); 
+curl_setopt($ch, CURLOPT_URL, 'http://88.247.29.227/test.php?link=https://admin-turkey.servicesadvisor.org/en/api/v1.0/service_location?fields=serviceName%26filter[id]=319'); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 $vars = json_decode($result, true);
